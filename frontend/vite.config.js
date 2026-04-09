366544a65e219cf47faecc582520bd6b11aab8c2
Anuradha godakanda <55067894+AnuradhaGP@users.noreply.github.com>
2026-04-09 13:24:43 +0530

    config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,6 +7,10 @@ export default defineConfig({
   server: {
     host: "0.0.0.0",
     port: 5173,
+    hmr: {
+      host: "bcai.duckdns.org",
+      protocol: "ws",
+    },
     strictPort: true,
   },
 });
