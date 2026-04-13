85978af6fdf5ef5c583972453927faf6319ca58e
Anuradha godakanda <55067894+AnuradhaGP@users.noreply.github.com>
2026-04-13 16:57:03 +0530

    u

--- backend/app.py
+++ backend/app.py
@@ -327,4 +327,4 @@ def get_monitoring_data():
     return jsonify({"data": realtime_data})
 
 if __name__ == '__main__':
-    socketio.run(app,host="0.0.0.0", port=5000, debug=True)
+    socketio.run(app,host="0.0.0.0", port=5000, debug=True,  allow_unsafe_werkzeug=True)
